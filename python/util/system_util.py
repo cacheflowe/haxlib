8531419b92ad3e4d806a04a7083a878eafd3aeaf
Justin Gitlin <349969+cacheflowe@users.noreply.github.com>
2026-07-26 18:34:39 -0400

    General updates

--- python/util/system_util.py
+++ python/util/system_util.py
@@ -24,3 +24,26 @@ def system_time():
 	current_time = datetime.now()
 	formatted_time = current_time.strftime("%H:%M:%S")
 	return formatted_time
+
+def print_python_info():
+	print(f'Python version: {sys.version}')
+	print(f'Python executable: {sys.executable}')
+	print(f'Python sys.path:')
+	for path in sys.path:
+		print(f'  - {path}')
+
+def print_numpy_info():
+	try:
+		import numpy as np
+		print(f'numpy version: {np.__version__}')
+		print(f'numpy location: {np.__file__}')
+	except ImportError:
+		print('numpy is not installed.')
+
+def print_pytorch_info():
+	try:
+		import torch
+		print(f'torch version: {torch.__version__}')
+		print(f'torch location: {torch.__file__}')
+	except ImportError:
+		print('torch is not installed.')
